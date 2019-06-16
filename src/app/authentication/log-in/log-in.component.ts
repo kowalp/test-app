3778fde66bc5c2de122ae08753bf307e978257c7
kowalp <38691024+kowalp@users.noreply.github.com>
2019-06-16 07:01:38 +0200

    fixing auth v2

--- src/app/authentication/log-in/log-in.component.ts
+++ src/app/authentication/log-in/log-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AutenthicationService } from 'src/app/shared/services/autenthication.service';
 
 @Component({
   selector: 'app-log-in',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-in.component.sass']
 })
 export class LogInComponent implements OnInit {
+  email = false;
+  passwordForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
-
+  constructor(private router: Router, private authService: AutenthicationService, private formBuilder: FormBuilder) { }
   ngOnInit() {
+    this.passwordForm = this.formBuilder.group({
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+    if (localStorage.getItem('email') !== null) {
+      this.email = true;
+    }
   }
-
+  get f() { return this.passwordForm.controls; }
+  onSubmit() {
+    this.submitted = true;
+    if (this.passwordForm.invalid) {
+      return;
+    }
+    this.authService.CheckPassword(this.passwordForm.value.password);
+  }
 }
--- src/app/shared/services/autenthication.service.ts
+++ src/app/shared/services/autenthication.service.ts
@@ -1,15 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/internal/Observable';
-
+import data from '../database.json';
+import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
 export class AutenthicationService {
+content = data;
+  constructor(private httpClient: HttpClient, private router: Router) {
+  }
 
-constructor(private httpClient: HttpClient) { }
-
-  CheckEmail(): Observable<any> {
-    return this.httpClient.get('../database.json');
+  CheckEmail(email) {
+    localStorage.removeItem('email');
+    for (const key in this.content) {
+      if (email === this.content[key].email) {
+        localStorage.setItem('email', email);
+        return this.router.navigate(['password']);
+      }
+    }
+  }
+  CheckPassword(password) {
+    for (const key in this.content) {
+      if (localStorage.getItem('email') === this.content[key].email) {
+        if (password === this.content[key].password) {
+            return this.router.navigate(['youtube']);
+        }
+      }
+    }
   }
 }
